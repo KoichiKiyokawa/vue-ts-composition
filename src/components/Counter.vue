52f58338e262d72bba4302c67357db2051c65608
K.Kiyokawa <koichi20110068@gmail.com>
2020-06-14 12:14:30 +0900

    feat: add counter component

---
+++ src/components/Counter.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+span {{title}}
+span {{count}}
+button(@click="increment") +1
+button(@click="decrement") -1
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+type Props = {
+  title: string
+}
+
+export default defineComponent({
+  props: { title: String },
+  setup() {
+    const count = ref(0)
+    const increment = () => {
+      count.value++
+    }
+    const decrement = () => {
+      if (count.value >= 1) count.value--
+    }
+
+    return { count, increment, decrement }
+  }
+})
+</script>

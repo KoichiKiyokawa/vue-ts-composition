2b0de01f3865e9256695dad56defd0a63ff3904f
K.Kiyokawa <koichi20110068@gmail.com>
2020-06-14 12:40:53 +0900

    feat: use tailwind in counter component

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,12 +1,16 @@
 <template lang="pug">
-span {{title}}
-span {{count}}
-button(@click="increment") +1
-button(@click="decrement") -1
+.max-w-sm.rounded.border-solide.shadow-lg.m-auto.flex.relative.items-center
+  span.px-4 {{title}}
+  span.text-lg.font-bold.px-4.absolute.centerize {{count}}
+  .ml-auto
+    button.bg-blue-500.rounded.text-white.py-1.px-3.m-1.text-lg(@click="increment") +
+    button.bg-red-500.rounded.text-white.py-1.px-3.m-1.text-lg(@click="decrement") -
+    span.text-gray-500.cursor-pointer.px-4 x
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
+
 type Props = {
   title: string
 }
@@ -26,3 +30,10 @@ export default defineComponent({
   }
 })
 </script>
+
+<style scoped>
+.centerize {
+  left: 50%;
+  transform: translateX(-50%);
+}
+</style>

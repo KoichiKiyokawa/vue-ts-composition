a1531827d18a384698c49474d48f6711a62a0dc7
K.Kiyokawa <koichi20110068@gmail.com>
2020-06-16 18:41:09 +0900

    feat: set tabindex -1 to buttons

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -3,9 +3,9 @@
   input.text-gray-600.mx-4.px-1.w-32(:value="title")
   span.text-lg.font-bold.px-4.absolute.centerize {{count}}
   .ml-auto
-    button.bg-red-500.rounded-l.text-white.py-1.px-3.my-1.text-lg(@click="increment") +
-    button.bg-blue-500.text-white.py-1.px-3.my-1.text-lg(@click="decrement") -
-    button.bg-yellow-500.rounded-r.text-white.py-1.px-3.my-1.text-lg(@click="reset") 0
+    button.bg-red-500.rounded-l.text-white.py-1.px-3.my-1.text-lg(tabindex="-1" @click="increment") +
+    button.bg-blue-500.text-white.py-1.px-3.my-1.text-lg(tabindex="-1" @click="decrement") -
+    button.bg-yellow-500.rounded-r.text-white.py-1.px-3.my-1.text-lg(tabindex="-1" @click="reset") 0
     span.text-gray-500.cursor-pointer.px-4(@click="$emit('close')") x
 </template>
 
